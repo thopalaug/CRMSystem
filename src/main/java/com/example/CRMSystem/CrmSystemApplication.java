0cc567f4b53add71a6c445e8be61b1a0d576dddc
Thomas Palme Augdal <thopalaug@gmail.com>
2022-04-08 11:58:18 +0200

    Initial commit

---
+++ src/main/java/com/example/CRMSystem/CrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/CRMSystem/util/ViewNames.java
@@ -0,0 +1,9 @@
+package com.example.CRMSystem.util;
+
+public final class ViewNames {
+
+    public static final String HOME = "home";
+
+    private ViewNames() {
+    }
+}
